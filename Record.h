d96ea7fcfd1df976a990104ab8f05f7387b2713e
NicolasArroyo <nicolas.arroyo.c@utec.edu.pe>
2022-10-14 15:35:59 -0500

    feat: creación de clase RecordBank, hija de Record

--- Record.h
+++ Record.h
@@ -9,4 +9,27 @@ struct Record {
     virtual std::string serialize() = 0;
 };
 
+struct RecordBank : public Record {
+    std::string sender_id;
+    std::string receiver_id;
+    double amount;
+    std::string date;
+
+    RecordBank(std::string sender_id, std::string receiver_id, double amount, std::string date) : sender_id(std::move(sender_id)),
+                                                                                                  receiver_id(std::move(receiver_id)),
+                                                                                                  amount(amount),
+                                                                                                  date(std::move(date)) {}
+
+    std::string serialize() override {
+        std::string formatted;
+
+        formatted += sender_id + ", ";
+        formatted += receiver_id + ", ";
+        formatted += std::to_string(amount) + ", ";
+        formatted += date;
+
+        return formatted;
+    }
+};
+
 #endif
